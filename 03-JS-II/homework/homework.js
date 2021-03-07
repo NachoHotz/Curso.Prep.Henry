b5b93d845b3450918c08eef7cb4903c997ec6977
Nacho Hotz <juanhotz611@gmail.com>
2021-03-07 18:54:14 -0300

    Modificado homework 03-JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -73,7 +73,6 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-
   if (numero === 10 || numero === 5) return true
 
   return false
