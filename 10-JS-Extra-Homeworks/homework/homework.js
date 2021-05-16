48f0ccc6c0ed1ccdbcd770c68c265f2bd3c7c398
Nacho Hotz <juanhotz611@gmail.com>
2021-05-16 11:22:04 -0300

    Done extra homework"

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -18,6 +18,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
   //Escribe tu código aquí
+  let counters = {};
+
+  string = string.split('');
+
+  for(let i = 0; i < string.length; i++){
+    if(!counters[string[i]]){
+        counters[string[i]] = 0;
+    }
+
+    counters[string[i]]++;
+  }
+
+  return counters;
 }
 
 function capToFront(s) {
